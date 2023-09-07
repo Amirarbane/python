cb24fa09c9b3cd2aeb9cf0c2c2bbd3b60450bec6
Amirarbane <144345021+Amirarbane@users.noreply.github.com>
2023-09-07 16:22:17 +0200

    Add files via upload

---
+++ chap2/exo2.py
@@ -0,0 +1,33 @@
+nom = 'Amir'
+langage = 'Python'
+
+print("\n1: #######################")
+
+for i in range(len(nom)):
+    print(nom[i])
+print("\n2: #######################")
+for i in range(len(langage)):
+    print(langage[i], end = '')
+    print("\n", len(langage))
+print("\n3: #######################")
+for i in range(5):
+    print(i)
+print("\n4: #######################")
+for i in range(5):
+    print("AMIR")
+print("\n5: #######################")
+for i in range(4,8):
+    print(i)
+print("\n6: #######################")
+for i in range(8,4,-1):
+    print(i)
+print("\n7: #######################")
+
+for i in range(0,10,2):
+    print(i)
+print("\n8: #######################")
+
+for element in langage:
+    print(element)
+
+   
