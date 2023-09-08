120f845eebe46374ec520403841a2d4a85062703
Amirarbane <144345021+Amirarbane@users.noreply.github.com>
2023-09-08 15:13:02 +0200

    Add files via upload

---
+++ exo4.py
@@ -0,0 +1,11 @@
+import random
+nb1 = random.randint(1,20)
+nb2 = random.randint(1,20)
+
+if  nb1 == nb2:
+
+    print(f"Félicitation {nb1} {nb2}  ")
+
+else:
+    
+    print(f" rater {nb1} {nb2}  ")
